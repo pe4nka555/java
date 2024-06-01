b41df1988460049b1cc556d18489e0b9292a9490
пккк <torr95157@gmail.com>
2024-06-01 04:13:29 +0300

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bicycle bicycle = new Bicycle("bmx", 2);
+        Car car = new Car("bmw", 4);
+        Truck truck = new Truck("truck", 6);
+
+        ServiceStation serviceStation = new ServiceStation();
+        serviceStation.check(bicycle, car, truck);
     }
 }
